48cad26b2b818413bfd978c8d6a0c7cd9dab7874
Marc Cavazos <marcavazos19@icloud.com>
2022-01-12 17:40:44 -0500

    commit tree 20ec267dfd8e7051f07e2286baadcfb587c74f36

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toUpperCase() === name.toUpperCase())
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.substring(0,letters.length) === letters)
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => name === driver.name)
+}
+
